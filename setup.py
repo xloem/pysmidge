226d13546d412faf0071dcc6c4794019218d33a6
xloem <0xloem@gmail.com>
2021-08-05 17:20:42 +0000

    license classifier

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     url='https://github.com/xloem/pysmidge',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GPL 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7'
